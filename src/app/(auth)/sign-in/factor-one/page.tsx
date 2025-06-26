41573c8cdfc56b402b8cd08911473922f8e914fe
Yhonny Fernandez <yhonny93@gmail.com>
2025-06-26 20:33:02 -0300

    arreglado factor one

--- src/app/(auth)/sign-in/factor-one/page.tsx
+++ src/app/(auth)/sign-in/factor-one/page.tsx
@@ -4,15 +4,23 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useUser } from "@clerk/nextjs";
 
+const ADMIN_CLERK_ID = "user_2yyZX2DgvOUrxDtPBU0tRHgxsXH";
+
 export default function RedirectFactorOne() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, isLoaded, user } = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    if (isSignedIn) {
-      router.replace("/dashboard");
+    if (!isLoaded) return;
+
+    if (isSignedIn && user) {
+      if (user.id === ADMIN_CLERK_ID) {
+        router.replace("/admin/orders");
+      } else {
+        router.replace("/dashboard");
+      }
     }
-  }, [isSignedIn]);
+  }, [isSignedIn, isLoaded, user, router]);
 
   return null; // pantalla vacía
 }
