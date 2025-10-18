f45f41ba32b68999f15c1fa317918da5b920028b
Yhonny Fernandez <yhonny93@gmail.com>
2025-10-18 10:46:42 -0300

    problemas pago movil

--- src/components/WarningBanner.tsx
+++ src/components/WarningBanner.tsx
@@ -3,7 +3,13 @@
 export default function WarningBanner() {
   return (
     <div className="z-50 w-full bg-yellow-900 text-yellow-100 px-4 py-3 rounded-xl border border-yellow-600 text-center shadow-lg max-w-3xl mx-auto mt-6">
-      ⚠️ PayPal está presentando problemas con su plataforma. El mantenimiento que están realizando afecta envíos y revisión de pagos. <strong>Pedimos tener paciencia.</strong> Gracias por la comprensión.
+      ⚠️ <strong>El sistema de Pago Móvil en BOLÍVARES</strong> está presentando fallas en algunos usuarios. 
+      Estamos realizando los pagos, pero se están tardando un poco más de lo normal. 
+      <br />
+      <br />
+      🟡 El problema es directamente del sistema <strong>PAGO MÓVIL</strong>, no tiene nada que ver con nosotros. 
+      <br />
+      🙏 Gracias por su comprensión.
     </div>
   );
 }
